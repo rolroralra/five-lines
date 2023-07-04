87720c38be0ecfae03fee92df44594788306f7a5
rolroralra <rolroralra@gmail.com>
2023-07-05 08:32:46 +0900

    feat: extract method

--- index.ts
+++ index.ts
@@ -80,92 +80,116 @@ function moveVertical(dy: number) {
   }
 }
 
-function update() {
+function handleInput(current: Input | Input.UP | Input.DOWN | Input.RIGHT) {
+  if (current === Input.LEFT)
+    moveHorizontal(-1);
+  else if (current === Input.RIGHT)
+    moveHorizontal(1);
+  else if (current === Input.UP)
+    moveVertical(-1);
+  else if (current === Input.DOWN)
+    moveVertical(1);
+}
+function handleInputs() {
   while (inputs.length > 0) {
     let current = inputs.pop();
-    if (current === Input.LEFT)
-      moveHorizontal(-1);
-    else if (current === Input.RIGHT)
-      moveHorizontal(1);
-    else if (current === Input.UP)
-      moveVertical(-1);
-    else if (current === Input.DOWN)
-      moveVertical(1);
+    handleInput(current);
   }
+}
 
+function updateMap() {
   for (let y = map.length - 1; y >= 0; y--) {
     for (let x = 0; x < map[y].length; x++) {
-      if ((map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE)
+      updateTile(y, x);
+    }
+  }
+
+  function updateTile(y: number, x: number) {
+    if ((map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE)
         && map[y + 1][x] === Tile.AIR) {
-        map[y + 1][x] = Tile.FALLING_STONE;
-        map[y][x] = Tile.AIR;
-      } else if ((map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX)
+      map[y + 1][x] = Tile.FALLING_STONE;
+      map[y][x] = Tile.AIR;
+    } else if ((map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX)
         && map[y + 1][x] === Tile.AIR) {
-        map[y + 1][x] = Tile.FALLING_BOX;
-        map[y][x] = Tile.AIR;
-      } else if (map[y][x] === Tile.FALLING_STONE) {
-        map[y][x] = Tile.STONE;
-      } else if (map[y][x] === Tile.FALLING_BOX) {
-        map[y][x] = Tile.BOX;
+      map[y + 1][x] = Tile.FALLING_BOX;
+      map[y][x] = Tile.AIR;
+    } else if (map[y][x] === Tile.FALLING_STONE) {
+      map[y][x] = Tile.STONE;
+    } else if (map[y][x] === Tile.FALLING_BOX) {
+      map[y][x] = Tile.BOX;
+    }
+  }
+
+  function createGraphics() {
+    let canvas = document.getElementById("GameCanvas") as HTMLCanvasElement;
+    let g = canvas.getContext("2d");
+
+    g.clearRect(0, 0, canvas.width, canvas.height);
+    return g;
+  }
+
+  function update() {
+    handleInputs();
+    updateMap();
+  }
+
+  function drawMap(g: CanvasRenderingContext2D) {
+    for (let y = 0; y < map.length; y++) {
+      for (let x = 0; x < map[y].length; x++) {
+        if (map[y][x] === Tile.FLUX)
+          g.fillStyle = "#ccffcc";
+        else if (map[y][x] === Tile.UNBREAKABLE)
+          g.fillStyle = "#999999";
+        else if (map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE)
+          g.fillStyle = "#0000cc";
+        else if (map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX)
+          g.fillStyle = "#8b4513";
+        else if (map[y][x] === Tile.KEY1 || map[y][x] === Tile.LOCK1)
+          g.fillStyle = "#ffcc00";
+        else if (map[y][x] === Tile.KEY2 || map[y][x] === Tile.LOCK2)
+          g.fillStyle = "#00ccff";
+
+        if (map[y][x] !== Tile.AIR && map[y][x] !== Tile.PLAYER)
+          g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
       }
     }
   }
-}
 
-function draw() {
-  let canvas = document.getElementById("GameCanvas") as HTMLCanvasElement;
-  let g = canvas.getContext("2d");
-
-  g.clearRect(0, 0, canvas.width, canvas.height);
-
-  // Draw map
-  for (let y = 0; y < map.length; y++) {
-    for (let x = 0; x < map[y].length; x++) {
-      if (map[y][x] === Tile.FLUX)
-        g.fillStyle = "#ccffcc";
-      else if (map[y][x] === Tile.UNBREAKABLE)
-        g.fillStyle = "#999999";
-      else if (map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE)
-        g.fillStyle = "#0000cc";
-      else if (map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX)
-        g.fillStyle = "#8b4513";
-      else if (map[y][x] === Tile.KEY1 || map[y][x] === Tile.LOCK1)
-        g.fillStyle = "#ffcc00";
-      else if (map[y][x] === Tile.KEY2 || map[y][x] === Tile.LOCK2)
-        g.fillStyle = "#00ccff";
-
-      if (map[y][x] !== Tile.AIR && map[y][x] !== Tile.PLAYER)
-        g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-    }
+  function drawPlayer(g: CanvasRenderingContext2D) {
+    g.fillStyle = "#000001";
+    g.fillRect(playerx * TILE_SIZE, playery * TILE_SIZE, TILE_SIZE, TILE_SIZE);
   }
 
-  // Draw player
-  g.fillStyle = "#ff0000";
-  g.fillRect(playerx * TILE_SIZE, playery * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+  function draw() {
+    let graphic = createGraphics();
+
+    drawMap(graphic);
+    drawPlayer(graphic);
+  }
+
+
+  function gameLoop() {
+    let before = Date.now();
+    update();
+    draw();
+    let after = Date.now();
+    let frameTime = after - before;
+    let sleep = SLEEP - frameTime;
+    setTimeout(() => gameLoop(), sleep);
+  }
+
+  window.onload = () => {
+    gameLoop();
+  }
+
+  const LEFT_KEY = "ArrowLeft";
+  const UP_KEY = "ArrowUp";
+  const RIGHT_KEY = "ArrowRight";
+  const DOWN_KEY = "ArrowDown";
+  window.addEventListener("keydown", e => {
+    if (e.key === LEFT_KEY || e.key === "a") inputs.push(Input.LEFT);
+    else if (e.key === UP_KEY || e.key === "w") inputs.push(Input.UP);
+    else if (e.key === RIGHT_KEY || e.key === "d") inputs.push(Input.RIGHT);
+    else if (e.key === DOWN_KEY || e.key === "s") inputs.push(Input.DOWN);
+  })
 }
-
-function gameLoop() {
-  let before = Date.now();
-  update();
-  draw();
-  let after = Date.now();
-  let frameTime = after - before;
-  let sleep = SLEEP - frameTime;
-  setTimeout(() => gameLoop(), sleep);
-}
-
-window.onload = () => {
-  gameLoop();
-}
-
-const LEFT_KEY = "ArrowLeft";
-const UP_KEY = "ArrowUp";
-const RIGHT_KEY = "ArrowRight";
-const DOWN_KEY = "ArrowDown";
-window.addEventListener("keydown", e => {
-  if (e.key === LEFT_KEY || e.key === "a") inputs.push(Input.LEFT);
-  else if (e.key === UP_KEY || e.key === "w") inputs.push(Input.UP);
-  else if (e.key === RIGHT_KEY || e.key === "d") inputs.push(Input.RIGHT);
-  else if (e.key === DOWN_KEY || e.key === "s") inputs.push(Input.DOWN);
-});
-
